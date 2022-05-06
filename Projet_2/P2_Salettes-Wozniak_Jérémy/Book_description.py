b30b1676bb50726746a0f06641b61de13994e534
Jeremy Salettes-Wozniak "AlchTech <HarmonyFidelis@gmail.com>
2022-05-06 23:00:40 +0200

    Génération des csv

--- Projet_2/P2_Salettes-Wozniak_Jérémy/Book_description.py
+++ Projet_2/P2_Salettes-Wozniak_Jérémy/Book_description.py
@@ -1,7 +1,11 @@
+from base64 import encode
+from pydoc import describe
+import shutil
 from webbrowser import get
 import requests
 from bs4 import BeautifulSoup
 import csv
+import os
 
 class Categories:
     def __init__(self, url):
@@ -50,13 +54,98 @@ class Books:
             dictionary_books[get_name] = list_books_by_category
 
             # PAGINATION
-            if get_page_soup.findAll('li', {'class': 'next'}) != []:
-                while get_page_soup.findAll('li', {'class': 'next'}) != []:
-                    get_next = get_page_soup.findAll('li', {'class': 'next'})[0].find('a').get('href')
-                    get_url = get_url[:-10]
-                    self.get_next = get_url  + get_next
-                    print(self.get_next)
+            # if get_page_soup.findAll('li', {'class': 'next'}) != []:
+            #     while get_page_soup.findAll('li', {'class': 'next'}) != []:
+            #         get_next = get_page_soup.findAll('li', {'class': 'next'})[0].find('a').get('href')
+            #         get_url = get_url[:-10]
+            #         self.get_next = get_url  + get_next
+            #         print(self.get_next)
         return dictionary_books                
             
 name_url_book = Books(name_url_categories).get_books()
-print(name_url_book)
+
+
+class Book_description:
+    
+    def __init__(self, url_book):
+        self.url_book = url_book
+        
+
+    def get_description(self):
+
+        for key, value in self.url_book.items():
+            list_description = []
+            for book in value:
+                get_page = requests.get(book)
+                get_soup = BeautifulSoup(get_page.content, 'html.parser')
+                select_info = get_soup.findAll('td')
+            
+                product_page_url = book
+                universal_product_code= select_info[0].get_text().strip()
+                title = get_soup.findAll('h1')[0].get_text().strip()
+                price_excluding_tax = select_info[2].get_text().strip().split('\n')
+                price_including_tax = select_info[3].get_text().strip().split('\n')
+                number_available = select_info[5].get_text().strip().split('\n')
+                product_description = get_soup.findAll('p')[3].get_text().strip()
+                category =  get_soup.findAll('a')[3].get_text().strip()
+                review_rating = select_info[6].get_text().strip().split('\n')
+                image_url = get_soup.findAll('img')[0].get('src')
+                image_url = "http://books.toscrape.com/media/" + image_url[12:]
+                
+                data = [
+                    product_page_url,
+                    universal_product_code,
+                    title,
+                    price_including_tax,
+                    price_excluding_tax,
+                    number_available,
+                    product_description,
+                    category,
+                    review_rating,
+                    image_url,
+                    ]
+                list_description.append(data)
+            self.description_csv(key, list_description)
+
+        
+
+      
+                
+            
+    def description_csv(self, name_category, data):
+        
+        # SI DOSSIER CSV EXISTE DEJA NE FAIRE RIEN
+        if os.path.exists(os.path.join(os.getcwd(), 'csv')):
+            pass
+        else:
+            os.mkdir(os.path.join(os.getcwd(), 'csv'))
+        
+        
+        fieldnames = [
+            'product_page_url',
+            'universal_product_code',
+            'title',
+            'price_including_tax',
+            'price_excluding_tax',
+            'number_available', 
+            'product_description', 
+            'category', 
+            'review_rating', 
+            'image_url'
+            ]
+        
+            
+        # ECRIRE DANS LE FICHIER CSV EN UTF-8
+        with open(name_category + '.csv', 'w', encoding='utf-8') as csv_file:
+            writer = csv.writer(csv_file, delimiter=',')
+            writer.writerow(fieldnames)
+            for row in data:
+                writer.writerow(row)
+        
+        source = r'./' + name_category + '.csv'
+        destination = r'./csv/' + name_category + '.csv'
+
+        shutil.move(source,destination)
+ 
+                
+Book_description(name_url_book).get_description()
