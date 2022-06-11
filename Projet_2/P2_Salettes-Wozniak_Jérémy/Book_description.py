4291e7b1243eef826a62552e6c5daebca820a5b7
Jeremy Salettes-Wozniak "AlchTech <HarmonyFidelis@gmail.com>
2022-06-11 11:40:43 +0200

    Pagination + formatage

--- Projet_2/P2_Salettes-Wozniak_Jérémy/Book_description.py
+++ Projet_2/P2_Salettes-Wozniak_Jérémy/Book_description.py
@@ -1,4 +1,7 @@
+# -*- coding: utf-8 -*-
+
 from base64 import encode
+from hashlib import new
 import os
 from pydoc import describe
 import shutil
@@ -7,6 +10,9 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import urllib.request
+
+
+
 
 class Categories:
     def __init__(self, url):
@@ -44,15 +50,17 @@ class Books:
                         next = get_soup_page.findAll('li', {'class': 'next'})[0].find('a').get('href')
                         next = value.replace("index.html", next)
                         url_tmp.append(next)
-                        
                 new_dictionary[key] = url_tmp
         return new_dictionary
 
         
     def get_books(self):
         pagination = self.pagination()
+        # PAGINATION RECUPERE CORRECTEMENT LES URLS DES PAGES
         dictionary_books = {}
         for key, value in pagination.items():
+            # RECUPERE LES URLS DES PAGES CORRECTEMENT
+            list_category = []
             for page in value:
                 list_books_by_category = []
                 get_page = requests.get(page)
@@ -66,8 +74,8 @@ class Books:
                         a = a[3:]
                     a =  "http://books.toscrape.com/catalogue/" + a
                     list_books_by_category.append(a)
-                dictionary_books[key] = list_books_by_category
-                
+                list_category.append(list_books_by_category)
+            dictionary_books[key] = list_category
         return dictionary_books          
             
 name_url_book = Books(name_url_categories).get_books()
@@ -79,61 +87,58 @@ class Book_description:
         self.url_book = url_book
 
     def get_description(self):
-        for key, value in self.url_book.items():
-            list_description = []
-            for book in value:
-                get_page = requests.get(book)
-                get_soup = BeautifulSoup(get_page.content, 'html.parser')
-                select_info = get_soup.findAll('td')
-                
-                product_page_url = book
-                universal_product_code= select_info[0].get_text().strip()
-                title = get_soup.findAll('h1')[0].get_text().strip()
-                price_excluding_tax = select_info[2].get_text().strip().split('\n')
-                price_including_tax = select_info[3].get_text().strip().split('\n')
-                number_available = select_info[5].get_text().strip().split('\n')
-                product_description = get_soup.findAll('p')[3].get_text().strip()
-                category =  get_soup.findAll('a')[3].get_text().strip()
-                review_rating = select_info[6].get_text().strip().split('\n')
-                image_url = get_soup.findAll('img')[0].get('src')
-                image_url = "http://books.toscrape.com/media/" + image_url[12:]
-                print(image_url)
-                
-                data = [
-                    product_page_url,
-                    universal_product_code,
-                    title,
-                    price_including_tax,
-                    price_excluding_tax,
-                    number_available,
-                    product_description,
-                    category,
-                    review_rating,
-                    image_url,
-                    ]
-                
-                
-                os.makedirs('./images/' + key, exist_ok=True)
-                urllib.request.urlretrieve(image_url, "./images/" + image_url[-12:])
-                shutil.move("./images/" + image_url[-12:], "./images/" + key)
-                # name_image = key + "_" + title + ".jpg"
-                # urllib.request.urlretrieve(image_url, name_image)
-                # # SI name_image ne contient pas .jpg print(image_url)
-                # if name_image.endswith(".jpg"):
-                #     print(image_url)
-                # shutil.move(name_image, './images/') 
+        for key, value in  self.url_book.items():
+            list_data = []
+            for list in value:
+                for element in list:
+                    get_page = requests.get(element)
+                    get_soup = BeautifulSoup(get_page.content, 'html.parser')
+                    select_info = get_soup.findAll('td')
                     
+                    product_page_url = element
+                    universal_product_code= select_info[0].get_text().strip()
+                    title = get_soup.findAll('h1')[0].get_text().strip()
+                    price_excluding_tax = select_info[2].get_text().strip().replace('(', '').replace(')', '')
+                    price_including_tax = select_info[3].get_text().strip().replace('(', '').replace(')', '')
+                    number_available = select_info[5].get_text().strip().replace("In stock (", "").replace("available)", "").replace(" ", "")
+                    product_description = get_soup.findAll('p')[3].contents[0].strip()
+                    category =  get_soup.findAll('a')[3].get_text().strip()
+                    review_rating = get_soup.findAll('p', {'class': 'star-rating'})[0].get('class')[1]
+                    image_url = get_soup.findAll('img')[0].get('src')
+                    image_url = "http://books.toscrape.com/media/" + image_url[12:]
                     
+                    data = [
+                        product_page_url,
+                        universal_product_code,
+                        title,
+                        price_including_tax,
+                        price_excluding_tax,
+                        number_available,
+                        product_description,
+                        category,
+                        review_rating,
+                        image_url,
+                        ]
                     
-                list_description.append(data)
-            self.description_csv(key, list_description)
+                    list_data.append(data)
+                
+                
+                    
+                    os.makedirs('./images/' + key, exist_ok=True)
+                    urllib.request.urlretrieve(image_url, "./images/" + image_url[-12:])
+                    shutil.move("./images/" + image_url[-12:], "./images/" + key)
+            
+           
+            
+            self.description_csv(key, list_data)
+            
 
         
 
       
                 
             
-    def description_csv(self, name_category, data):
+    def description_csv(self, name_category, list_data):
         fieldnames = [
             'product_page_url',
             'universal_product_code',
@@ -147,11 +152,14 @@ class Book_description:
             'image_url'
             ]
 
+        print(name_category)
+        print(list_data)
         with open(name_category + '.csv', 'w', encoding='utf-8') as csvfile:
             writer = csv.writer(csvfile, delimiter=',')
             writer.writerow(fieldnames)
-            for row in data:
-                writer.writerow(row)
+            for data in list_data:
+                # for row in data:
+                writer.writerow(data)
         
             
         os.makedirs("./csv/" + name_category, exist_ok=False)
